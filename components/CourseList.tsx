e7d89f8d5562e27a8e25174021d0d50346b13c3b
1119048735 <61829520+1119048735@users.noreply.github.com>
2026-07-16 14:52:36 +0800

    Update CourseList.tsx

--- components/CourseList.tsx
+++ components/CourseList.tsx
@@ -6,7 +6,67 @@ export function CourseList() {
 
     <div>
 
-      课程权限测试成功
+      'use client'
+
+import { useEffect, useState } from 'react'
+import { courses } from '@/lib/courses'
+
+
+export function CourseList() {
+
+
+  const [userCourse, setUserCourse] = useState("")
+
+
+  useEffect(() => {
+
+    const user = localStorage.getItem("user")
+
+
+    if(user){
+
+      const data = JSON.parse(user)
+
+      setUserCourse(data.course)
+
+    }
+
+  }, [])
+
+
+
+  return (
+
+    <div>
+
+      当前购买权限：
+
+      {userCourse}
+
+
+      <hr />
+
+
+      {
+
+        courses.map((course)=>(
+
+          <div key={course.id}>
+
+            {course.name}
+
+          </div>
+
+        ))
+
+      }
+
+
+    </div>
+
+  )
+
+}
 
     </div>
 

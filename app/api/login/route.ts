8334f07af627003d306f90a17d571b652d6c58d0
1119048735 <61829520+1119048735@users.noreply.github.com>
2026-07-16 15:26:45 +0800

    Update route.ts

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,53 +1,119 @@
 import { NextResponse } from "next/server"
 import orders from "@/data/orders.json"
+import products from "@/data/products.json"
 
 
 export async function POST(request: Request) {
 
   try {
 
+
     const { orderId } = await request.json()
 
 
-    const user = orders.find(
-      item =>
-        item.orderId === orderId &&
-        item.status === "active"
-    )
-
-
-    if (!user) {
+    if (!orderId) {
 
       return NextResponse.json({
-        success:false,
-        message:"订单不存在"
+        success: false,
+        message: "请输入订单编号"
       })
 
     }
 
 
+
+    // 查找订单
+
+    const order = orders.find(
+
+      item =>
+        item.orderId === orderId &&
+        item.status === "active"
+
+    )
+
+
+
+    if (!order) {
+
+      return NextResponse.json({
+
+        success: false,
+
+        message: "订单不存在"
+
+      })
+
+    }
+
+
+
+    // 根据订单找到套餐
+
+    const product = products.find(
+
+      item =>
+        item.productId === order.productId
+
+    )
+
+
+
+    if (!product) {
+
+
+      return NextResponse.json({
+
+        success:false,
+
+        message:"购买套餐不存在"
+
+      })
+
+
+    }
+
+
+
     return NextResponse.json({
+
 
       success:true,
 
+
       user:{
-        name:user.name,
-        course:user.course,
-        expire:user.expire
+
+
+        name: order.name,
+
+
+        product: product.name,
+
+
+        courses: product.courses,
+
+
+        expire: order.expire
+
+
       }
+
 
     })
 
 
-  } catch {
+
+  } catch(error) {
 
 
     return NextResponse.json({
 
       success:false,
+
       message:"服务器错误"
 
     })
+
 
   }
 
